fe52b5938885b712755272fc2d2881ee8bb4235d
nicholas_zhu <nic19999@126.com>
2024-10-25 03:23:58 +0800

    feat 侧边栏固定

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -83,43 +83,51 @@ export default function ListLayoutWithTags({
             {title}
           </h1>
         </div>
-        <div className="flex sm:space-x-24">
-          <div className="hidden h-full max-h-screen min-w-[280px] max-w-[280px] flex-wrap overflow-auto rounded bg-gray-50 pt-5 shadow-md dark:bg-gray-900/70 dark:shadow-gray-800/40 sm:flex">
-            <div className="px-6 py-4">
-              {pathname.startsWith('/blog') ? (
-                <h3 className="font-bold uppercase text-primary-500">All Posts</h3>
-              ) : (
-                <Link
-                  href={`/blog`}
-                  className="font-bold uppercase text-gray-700 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-500"
-                >
-                  All Posts
-                </Link>
-              )}
-              <ul>
-                {sortedTags.map((t) => {
-                  return (
-                    <li key={t} className="my-3">
-                      {decodeURI(pathname.split('/tags/')[1]) === slug(t) ? (
-                        <h3 className="inline px-3 py-2 text-sm font-bold uppercase text-primary-500">
-                          {`${t} (${tagCounts[t]})`}
-                        </h3>
-                      ) : (
-                        <Link
-                          href={`/tags/${slug(t)}`}
-                          className="px-3 py-2 text-sm font-medium uppercase text-gray-500 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-500"
-                          aria-label={`View posts tagged ${t}`}
-                        >
-                          {`${t} (${tagCounts[t]})`}
-                        </Link>
-                      )}
-                    </li>
-                  )
-                })}
-              </ul>
+        <div className="flex sm:space-x-24 lg:grid lg:grid-cols-5 lg:gap-x-6">
+          {/* 侧边栏 */}
+          <aside>
+            {/* <div className="hidden lg:sticky  min-w-[280px] max-w-[280px] 
+             rounded bg-gray-50 pt-5 shadow-md dark:bg-gray-900/70 dark:shadow-gray-800/40 lg:col-span-1 lg:block"> */}
+            <div className="hidden lg:sticky lg:top-10 lg:col-span-1 lg:block min-w-[280px] max-w-[280px] 
+             rounded bg-gray-50 shadow-md dark:bg-gray-900/70 dark:shadow-gray-800/40">
+              <div className="px-6 py-4">
+                {pathname.startsWith('/blog') ? (
+                  <h3 className="font-bold uppercase text-primary-500">All Posts</h3>
+                ) : (
+                  <Link
+                    href={`/blog`}
+                    className="font-bold uppercase text-gray-700 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-500"
+                  >
+                    All Posts
+                  </Link>
+                )}
+                <ul>
+                  {sortedTags.map((t) => {
+                    return (
+                      <li key={t} className="my-3">
+                        {decodeURI(pathname.split('/tags/')[1]) === slug(t) ? (
+                          <h3 className="inline px-3 py-2 text-sm font-bold uppercase text-primary-500">
+                            {`${t} (${tagCounts[t]})`}
+                          </h3>
+                        ) : (
+                          <Link
+                            href={`/tags/${slug(t)}`}
+                            className="px-3 py-2 text-sm font-medium uppercase text-gray-500 hover:text-primary-500 dark:text-gray-300 dark:hover:text-primary-500"
+                            aria-label={`View posts tagged ${t}`}
+                          >
+                            {`${t} (${tagCounts[t]})`}
+                          </Link>
+                        )}
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
             </div>
-          </div>
-          <div>
+          </aside>
+
+          {/* 文章列表 */}
+          <div className="divide-y divide-gray-200 pb-8 transition-colors dark:divide-gray-700 lg:col-span-4">
             <ul>
               {displayPosts.map((post) => {
                 const { path, date, title, summary, tags } = post
